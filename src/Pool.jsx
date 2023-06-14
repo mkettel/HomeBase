fc9cc2b2bbaa3d97d33837985717d4acca5bbb9c
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-06-14 17:02:32 -0500

    added the changes and a video to the main page

--- src/Pool.jsx
+++ src/Pool.jsx
@@ -8,33 +8,11 @@ import { useState } from 'react';
 
 export default function Pool() {
 
-  const poolVideos = [
-    {
-      id: 1,
-      url: './videos/pool/cl-floating-chamber.mp4',
-      title: 'Floating Chlorine Chamber'
-    },
-    {
-      id: 2,
-      url: 'https://www.youtube.com/embed/BKxCOasoqsc',
-      title: 'Backwashing the Pool Pump'
-    },
-    {
-      id: 3,
-      url: './videos/pool/cl-floating-chamber.mp4',
-      title: 'Pool Pump Breaker'
-    },
-    {
-      id: 4,
-      url: './videos/pool/cl-floating-chamber.mp4',
-      title: 'Pool Pump Pressure'
-    }
-  ]
 
   const [season, setSeason] = useState('season');
 
 
-  // Setting season state
+  // Setting season state for proper component render
   if (season === 'season') {
     return (
       <SeasonSelector season={season} setSeason={setSeason} />
@@ -50,18 +28,7 @@ export default function Pool() {
   }
 
 
-    {/* <div className="home-link">
-      <a href="/">House</a>
-    </div>
-
-    <div className="header-container">
-      <div className="header">
-        <div className="header-text">
-          <h1>Pool & Hot Tub</h1>
-        </div>
-      </div>
-    </div>
-
+    {/*
     <div className="content-container-outer">
       <div className="content-container center-title">
         <h2 className='subject-title'>{poolVideos[1].title}</h2>
@@ -115,27 +82,77 @@ export function SeasonSelector(props) {
       <span className='summer-span'>r</span>
       </button>
     </div>
+  </>
+}
 
+
+// Spring Videos
+export function SpringVideos(props) {
+
+  return <>
+  <div className="page-container">
+    <h2>Videos for Spring Pool action</h2>
+    <p>Under Construction Check Back Soon</p>
+    <button onClick={() => props.setSeason('season')}>Back to Seasons</button>
+  </div>
   </>
 }
 
 // Summer Videos
 export function SummerVideos(props) {
 
+  const summerPoolVideos = [
+    {
+      id: 1,
+      url: './videos/pool/cl-floating-chamber.mp4',
+      title: 'Floating Chlorine Chamber'
+    },
+    {
+      id: 2,
+      url: 'https://www.youtube.com/embed/BKxCOasoqsc',
+      title: 'Backwashing the Pool Pump'
+    },
+    {
+      id: 3,
+      url: './videos/pool/cl-floating-chamber.mp4',
+      title: 'Pool Pump Breaker'
+    },
+    {
+      id: 4,
+      url: './videos/pool/cl-floating-chamber.mp4',
+      title: 'Pool Pump Pressure'
+    }
+  ]
 
   return <>
-    <h2>Videos for Summer Pool action</h2>
-    <button onClick={() => props.setSeason('season')}>seasons</button>
+    <button className='seasons-back-button' onClick={() => props.setSeason('season')}>Back to Seasons</button>
 
-  </>
-}
+    <div className="header-container">
+      <h1 className='header-container-title'>Summer Pool & Hot Tub Videos</h1>
+    </div>
 
-// Spring Videos
-export function SpringVideos(props) {
+    <div className="content-container-outer">
+      <div className="content-container center-title">
+        <h2 className='subject-title'>{summerPoolVideos[1].title}</h2>
+      </div>
+      <div className="content-container-inner">
+        <div className="content-container-video">
+          <iframe width="300" height="315" src={summerPoolVideos[1].url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen ></iframe>
+        </div>
+        <div className="content-container-description">
+          <div className="description-summary">
+            <p>The floating chlorine chamber is used to slowly release the chlorine into the pool over time and can hold up to 6 tablets at one time</p>
+          </div>
+          <div className="description-steps">
+            <ol>
+              <li>Open the floating chlorine chamber</li>
+              <li>Place 6 chlorine tablets into the chamber as needed</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+      </div>
 
 
-  return <>
-    <h2>Videos for Spring Pool action</h2>
-    <button onClick={() => props.setSeason('season')}>seasons</button>
   </>
 }
