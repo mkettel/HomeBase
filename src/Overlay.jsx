8c8824999d74523bf96f20fee4be8a851dced91a
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-06-14 23:20:53 -0500

    added the homebase logo thing to the homepage and will add some more changes here soon

--- src/Overlay.jsx
+++ src/Overlay.jsx
@@ -7,8 +7,23 @@ export default function Overlay() {
 
   return <>
 
-    <div className="test-button">
+    <div className="pool-button">
       <a className='nav' href="/pool">Pool</a>
     </div>
+
+
+    <div className="yard-button">
+      <a className='nav' href="/pool">Yard</a>
+    </div>
+
+    {/* Description */}
+    <div className="overlay-description">
+      <p className='main-header' href="/pool">HomeBase</p>
+    </div>
+
+
+
+
+
   </>
 }
